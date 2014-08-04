dff4441f2fa394856d19b616d4e403078e923207
Jonathan Gamble <jdgamble555@gmail.com>
2014-08-04 01:22:41 -0700

    minor changes

--- word_tools.cpp
+++ word_tools.cpp
@@ -2,7 +2,7 @@
 ** Filename: word_tools.cpp
 ** Date: 7/17/2014
 ** Updated: 8/1/2014
-** Version: 2.2.2
+** Version: 2.3.0
 ** Description: Functions for words
 ** Contributors: Michael Hoppes
 **               Jonathan Gamble
@@ -18,11 +18,17 @@ using std::cout;
 using std::endl;
 using std::ifstream;
 
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+
 namespace word_tools {
 
   void clear_screen() {
     // clear the screen
-    cout << string(100, '\n');
+    system(CLEAR);
   }
 
   void display_bar() {
@@ -65,20 +71,20 @@ namespace word_tools {
     return rand() % range + min;
   }
 
+  void replace_all_chars(char c, string str1, string &str2) {
+    // replace all matching char occurences from str1 in str2
+    for (size_t i = 0; i < str1.length(); i++) {
+      if (c == str1.at(i))
+        str2[i] = c;
+    }
+  }
+
   bool is_in_array(int array[], int size, int search) {
     // check if int is in array of int
     for (int i = 0; i < size; i++)
       if (array[i] == search)
         return true;
     return false;
-  }
-
-  void replace_blank_word(char letter, std::string word, std::string &blank_word) {
-    // replace all occurences char in blank_word string
-    for (size_t i = 0; i < word.length(); i++) {
-      if (letter == word.at(i))
-        blank_word.replace(i, 1, std::string(1, letter));
-    }
   }
 
   bool is_char_in_array(char c, char *arr, int size) {
@@ -89,18 +95,23 @@ namespace word_tools {
     return false;
   }
 
-  bool is_char_in_str(char c, std::string s) {
+  bool is_char_in_str(char c, string s) {
     // see if character is in the string
-    return (s.find(c) != std::string::npos);
+    return (s.find(c) != string::npos);
   }
 
-  int num_char_in_str(char c, std::string s) {
+  int num_char_in_str(char c, string s) {
     // return number of characters in a string
     int n = 0;
     for (size_t i = 0; i < s.length(); i++)
       if (s.at(i) == c)
         n++;
     return n;
+  }
+
+  bool are_equal(char a, char b, char c) {
+    // see if three characters are equal
+    return (a == b && b == c);
   }
 
 }
